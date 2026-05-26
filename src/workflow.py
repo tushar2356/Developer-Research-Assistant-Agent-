0413b04b2aec7740b89d584817f4e9e5704afc97
tushar2356 <tushargehlot2489@gmail.com>
2026-05-26 10:28:06 +0530

    migrated to groq

--- src/workflow.py
+++ src/workflow.py
@@ -6,14 +6,14 @@ from src.models import ResearchState, CompanyInfo, CompanyAnalysis
 from src.firecrawl import FirecrawlService
 from src.prompts import DeveloperToolsPrompts
 import os
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
 class Workflow:
     def __init__(self):
         self.firecrawl = FirecrawlService()
-        self.llm = ChatGoogleGenerativeAI(
-                model="gemini-1.5-flash",
-                temperature=0.1,
-                google_api_key=os.getenv("GEMINI_API_KEY")
+        self.llm = ChatGroq(
+            model="llama-3.3-70b-versatile",
+            temperature=0.1,
+            groq_api_key=os.getenv("GROQ_API_KEY")
         )
         self.prompts = DeveloperToolsPrompts()
         self.workflow = self._build_workflow()
